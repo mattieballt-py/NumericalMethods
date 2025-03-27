918b811a0295b3ab4f95478cf3c12955d5f9540b
Mattie <Mbt.jghs@gmail.com>
2025-03-27 12:48:59 +0100

    golden search agorithm working

--- Optimisation.py
+++ Optimisation.py
@@ -2,18 +2,54 @@
 import numpy as np
 
 """
-Golden Search Method
+Golden Search Method to find max/min of a function 
 """
 # want to find maximum of a function using only one func eval per iteration
 
 # the function we want to search for a max in
-def df(x):
+def df(x): # will find where df/dx = 0
     return -2*x*np.exp((-x**2))
 
-# search interval
-xl = 0 # lower boundary
-xu = 10 # upper boundary
+#not recursively
+def goldsrch(xl,xu,tol):
+    R = (np.sqrt(5) - 1) / 2  # Golden ratio
+    e = 100 # set high initial error to start off loop
+    i = 0
+    # just to start it off:
+    x1 = xl + (xu-xl)*R # compute x1
+    x2 = xu - (xu-xl)*R # compute x2 
+    fx1 = df(x1)
+    fx2 = df(x2)
+    while abs(e) > tol:
+        xn = (xl + xu)/2 # before iteration best guess
+        print("xl:",xl,"xu:",xu)
+        print("df(xl):",df(xl),"df(xu):",df(xu))
+        if fx1 < fx2:
+            i += 1
+            xl = x2
+            x2 = x1
+            #x2' = x1
+            x1 = xl + (xu-xl)*R # compute x1
+            fx2 = fx1 # and no need to recalculate fx2  
+            fx1 = df(x1)            
+        else: # fx2 < fx1:
+            i += 1
+            xu = x1
+            x1 = x2
+            #x1' = x2
+            fx1 = fx2 # and no need to recalculate fx1
+            x2 = xu - (xu-xl)*R # compute x2 
+            fx2 = df(x2)
+        e = (((xl+xu)/2) - xn)/((xl+xu)/2) # compute width => error (xn+1 - xn)/xn
+        #print("new best guess at (n + 1), i: ",i,"is: ", (xl+xu)/2)
+    print("best guess under tol where derivative=0:",(xl+xu)/2)
+    return (xl+xu)/2
+        
+# or
 
+# recursively:
+"""
+Needs fixing
 def goldsearch(xl,xu,i):
     xl = xl
     xu = xu
@@ -40,8 +76,10 @@ def goldsearch(xl,xu,i):
             i += 1
             print(i)
             return goldsearch(x2, xu,i)  # Shrink the interval towards x1
+"""
 
+#print(goldsrch(0,1,0.01))
 
-
-print(goldsearch(0,1,0))
-
+"""
+Steepest Ascent Method
+"""
