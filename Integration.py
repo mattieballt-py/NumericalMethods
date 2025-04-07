1f099478b3ce7083d47af2b57dc3444166898e0a
Mattie <Mbt.jghs@gmail.com>
2025-04-07 20:13:28 +0100

    Adaptive Simpsons 1/3rd Integration Done

--- Integration.py
+++ Integration.py
@@ -3,16 +3,17 @@ import matplotlib.pyplot as plt
 
 
 """
-Integration with Guass Quadrature
+Integration with Guass Quadrature 
 """
+# when you (know the function but no known points)
 
-def f(t):
+def g(t):
     A = 0
     B = 1
-    x = 0.5*(A * (1-t) + B*(t + 1))
+    x = 0.5*(A * (1-t) + B*(t + 1)) # A is lower limit, B is upper
     return x * np.exp(x**2)
 
-def GuassQuad(f,n): # n is degree of precision
+def GuassQuad(g,n): # n is degree of precision
     nodes = [] # = tj
     weights = [] # = wj
     I = 0 # just to init
@@ -29,18 +30,78 @@ def GuassQuad(f,n): # n is degree of precision
         weights.append((18 - 30**0.5)/36)
         weights.append((18 + 30**0.5)/36)
         weights.append((18 + 30**0.5)/36)
-        print(nodes, weights)
+
         # run the integration
         for j in range(0,4):
-            I += weights[j]*f(nodes[j])
+            I += weights[j]*g(nodes[j])
         I *= 0.5
         return I
     else:
         return
 
-print("Guass solution:", GuassQuad(f,7))
-"""
-Integration with Simpson's 1/3 rule
-"""
+print("Guass solution:", GuassQuad(g,7))
 
 
+"""
+Integration of f(x) for a spacing of h between A and B with known points
+"""
+A = 0
+B = 1
+N = 10 # total number of known points
+h = (B-A)/N
+
+x_known = np.linspace(A,B,N)
+
+def f(x_known):
+    return x_known * np.exp(x_known**2)
+
+y_known = f(x_known)
+
+"""
+Simpsons 1/3 method
+"""
+
+def Simp13(y_known, N, h):
+    I_even = 0 # init
+    I_odd = 0 
+    I = y_known[0] + y_known[-1]
+    for n in range(0,N):
+        if n%2 == 0: # n is even
+            I_even += y_known[n]
+        else:
+            I_odd += y_known[n]
+    I = h/3 * (I + 4*I_odd + 2*I_even)
+    return I
+
+print("simp", Simp13(y_known,N,h))
+
+"""
+Adaptive Simpsons 1/3 method
+"""
+# if you need to get I to be more accurate, reduce h:
+
+def Simp13adaptive(y_known, N, h,tol):
+
+    I_even = 0 # init
+    I_odd = 0 
+    I = y_known[0] + y_known[-1]
+    for n in range(0,N):
+        if n%2 == 0: # n is even
+            I_even += y_known[n]
+        else:
+            I_odd += y_known[n]
+    I = h/3 * (I + 4*I_odd + 2*I_even)
+
+    act = GuassQuad(g,7) # as its the same function
+    err = (act - I)/act
+    print("err",err)
+    if abs(err) > tol:
+        N += 100 # increase number of points
+        x_known = np.linspace(A,B,N) # recalc x
+        y_known = f(x_known) # refind more y values
+        h = x_known[1] - x_known[0]
+        print("h: ",h)
+        return Simp13adaptive(y_known,N,h,tol)
+    return I
+
+print("simpadapt", Simp13adaptive(y_known,N,h,0.00005), "vs guass quad: ", GuassQuad(g,7))
