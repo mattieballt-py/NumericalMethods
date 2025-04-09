d894ff4e7db4f9abc83509a1ada5e153990e50d2
Mattie <Mbt.jghs@gmail.com>
2025-04-09 10:22:13 +0100

    Explicit solution for WaveEQN

--- PDE_Explicit.py
+++ PDE_Explicit.py
@@ -18,7 +18,7 @@ T = np.zeros((len(t),len(x))) # initialise Temp arra
 T[0,:] = 1
 
 
-def explicit(x,t,T,r): # with BC's set up as tutorial question
+def explicitDiff(x,t,T,r): # with BC's set up as tutorial question
     
     for i in range(0,len(t)-1): # so at each time step
         # BC's:
@@ -33,7 +33,7 @@ def explicit(x,t,T,r): # with BC's set up as tutorial question
         T[i+1,-1] = 0
     return T
 
-diff = explicit(x,t,T,r)
+diff = explicitDiff(x,t,T,r)
 
 
 # Plot results
@@ -46,3 +46,53 @@ plt.title('Heat Equation Solution - Explicit Method')
 plt.legend()
 plt.grid(True)
 #plt.show()
+
+
+"""
+Explicit solution Wave Eqn Non-Dimensionalised
+"""
+# explicit solution of d^2T/dx^2 - d^2T/dt^2 = 0  non dimensionalised
+
+dx = 0.2 # space step
+dt = 0.2 # time step
+r = 1 # r = 1 for this
+#print("convergence factor r",r)
+
+x = np.arange(0,1+dx,dx) # mesh in x here gives 0, 0.2, 0.4, 0.6, 0.8, 1
+t = np.arange(0,2+dt,dt) # mesh in time up to 2 seconds
+U = np.zeros((len(t),len(x))) # initialise u array, time, space
+
+# BC's:
+U[:,0] = 0
+U[0,:] = np.sin((np.pi)*x) # at t=0, u = sinwx
+U[:,-1] = 0
+U[1,:] = U[0,:]# because newman bc du/dt = 0 at t=0
+
+def explicitWave(x,t,U,r): # with BC's set up as tutorial question
+    
+    for i in range(2,len(t)): # so at each time step   
+        for j in range(1,len(x)-1): # from LHS to RHS stepping along x
+            U[i,j] = U[i-1,j-1] + U[i-1,j+1] - U[i-2,j]
+
+        # reapply BC's to next step:
+        U[:,0] = 0
+        U[0,:] = np.sin((np.pi)*x) # at t=0, u = sinwx
+        U[:,-1] = 0
+        U[1,:] = U[0,:]# because newman bc du/dt = 0 at t=0
+
+    return U
+
+diff = explicitWave(x,t,U,r)
+print(diff)
+
+# Plot results
+plt.figure(figsize=(10, 6))
+for n in [0, int(len(t)/4), int(len(t)/2), len(t)-1]:
+    plt.plot(x, diff[n, :], label=f't = {t[n]:.2f}')
+plt.xlabel('x')
+plt.ylabel('wave position')
+plt.title('Wave Equation Solution - Explicit Method')
+plt.legend()
+plt.grid(True)
+plt.show()
+
