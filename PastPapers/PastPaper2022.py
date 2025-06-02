362e6ab83601534f5fdbce805954db6cd8119ec5
Mattie <Mbt.jghs@gmail.com>
2025-06-02 20:12:42 +0100

    2022 Paper Lagrangian

--- PastPapers/PastPaper2022.py
+++ PastPapers/PastPaper2022.py
@@ -59,3 +59,158 @@ xA = f1(zA)  # or f2(zA), both give x
 print("intersection at xA,zA",xA,zA)
 
 
+"""
+Task B: Heat Diffusion PDE
+"""
+
+# this specific solution is from the ice cream Q, adjust paramaters, bc's etc as needed:
+# spatial domain (in mm)
+R = 30
+dx = 0.5 # spatial increment
+# greate the spatial grid points
+x = np.arange(-R,R+dx,dx)
+Nx = len(x)
+
+# temporal domain
+tend = 3600
+dt = 1 # temporal increment
+# create the temporal grid points
+t = np.arange(0,tend+dt,dt)
+Nt = len(t)
+
+# create the solution matrix
+T = np.ndarray((Nt,Nx))
+
+# set the physics
+# boundary conditions (fixed temperature at boundaries)
+Ta = 35
+Tb = 35
+# set the initial T for watermelon
+T[0,:int(Nx/2)] = 2
+# set the initial T for pistachio
+T[0,int(Nx/2):] = 6
+
+# set the thermal diffusivity
+alpha = np.ndarray(len(x))
+# set the diffusivity T for watermelon
+alpha[:int(Nx/2)] = 4.5e-2
+# set the diffusivity for pistachio
+alpha[int(Nx/2):] = 8.6e-2
+# ================================================
+
+averagep = np.average(T[0,int(Nx/2):])
+averagew = np.average(T[0,:int(Nx/2)])
+avp = [averagep]
+avw = [averagew]
+Tmelt = 15
+
+c = alpha * dt / dx**2
+
+# compute the solution incrementally at subsequent time steps
+k = 1
+while averagep < Tmelt:
+    # compute at time step p, i.e. t = k * dt
+    # do it for every node in the spatial grid
+    # start with the boundaries
+    T[k,0] = Ta
+    T[k,-1] = Tb
+    # do the interior nodes
+    for i in range(1,Nx-1):
+        # apply the discretised equation
+        T[k,i] = c[i] * ( T[k-1,i+1] + T[k-1,i-1] ) + (1 - 2*c[i]) * T[k-1,i]
+    averagep = np.average( T[k,int(Nx/2):])
+    averagew = np.average( T[k,:int(Nx/2)])
+    avp += [averagep]
+    avw += [averagew]
+    k += 1
+
+
+plt.plot(x,T[k-1,:])
+plt.grid()
+plt.show()
+
+
+plt.plot(t[:k],avp,c='Green')
+plt.plot(t[:k],avw,c='Red')
+plt.grid()
+plt.show()
+
+print('Ice cream melted after minutes:')
+print((k-1)*dt/60)
+
+print('Courant:')
+print(c[-1],c[0])
+
+"""
+Task C: Lagrangian Interpolation of randomly spaced co ords for a logo
+"""
+
+# opening and cleaning x vals of the logo
+with open('/Users/hedgehog/Desktop/MechEng_Degree/ME2_All/Computing_ME2/Python_ME2/NumericalMethods/NumericalMethods/txt_Data/LogoXn.txt', 'r') as d:
+    xvals = d.readlines()
+
+# Initialize lists to store cleaned float values
+xnknown = []
+for item in xvals:
+    term = item.strip()
+    try:
+        clean_term = float(term)
+        xnknown.append((clean_term))
+    except ValueError:
+        continue
+
+xn = np.array(xnknown)
+print("xn", xn)
+
+with open('/Users/hedgehog/Desktop/MechEng_Degree/ME2_All/Computing_ME2/Python_ME2/NumericalMethods/NumericalMethods/txt_Data/LogoYn.txt', 'r') as d:
+    yvals = d.readlines()
+
+# Initialize lists to store cleaned float values
+ynknown = []
+for item in yvals:
+    term = item.strip()
+    try:
+        clean_term = float(term)
+        ynknown.append(clean_term)
+    except ValueError:
+        continue
+yn = np.array(ynknown)
+print("yn", yn)
+
+
+# Interpolation using Lagrangian method
+def Langrangian(x_known, y_known, xp):
+    n = len(x_known)
+    P = 0
+    for i in range(n):
+        Li = 1
+        for j in range(n):
+            if i != j:
+                Li *= (xp - x_known[j]) / (x_known[i] - x_known[j])
+        P += y_known[i] * Li
+    return P
+
+def InterpPoints(x_known, y_known, InterpMethod, xp_points):
+    yp_points = xp_points.copy()
+    for i in range(len(xp_points)):
+        yp_points[i] = InterpMethod(x_known, y_known, xp_points[i])
+    return yp_points
+
+# Bracketing interval between t = 3 and t = 3.5
+x_interp = np.arange(-19, 19+0.5, 0.5)
+Temp_interp = InterpPoints(xn, yn, Langrangian, x_interp)
+
+# Plotting interpolated section
+plt.figure(figsize=(8, 4))
+plt.plot(x_interp, Temp_interp, color='black')
+plt.scatter(xn, yn, color='red',s = 50) # make smaller so can see interp points
+plt.title("Interpolated points for root finding")
+plt.xlabel("x")
+plt.ylabel("y(x)")
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+"""
+
+"""
